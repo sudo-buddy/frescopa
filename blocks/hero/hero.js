0a78a162bb5281790168bbf0be218b65125b55db
Bhanu Dash <107146107+sudo-buddy@users.noreply.github.com>
2026-07-28 15:37:13 -0700

    Add data-target-scope to hero block for Form-based Target activity testing

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,4 +1,5 @@
 export default function decorate(block) {
+  block.setAttribute('data-target-scope', 'hero-mbox');
   const buttonContainers = [...block.querySelectorAll('.button-container')];
   if (buttonContainers.length > 1) {
     const wrapper = document.createElement('p');
